b0daa2ee93ab669628d8f4b148e7726b3b8afe03
Jake Tronge <jtronge3@gmail.com>
2024-04-18 11:17:33 -0600

    Add experimental API to header file

--- mpicd-capi/include/mpi.h
+++ mpicd-capi/include/mpi.h
@@ -56,6 +56,52 @@ int MPI_Type_create_custom(MPI_Type_custom_pack_function *packfn,
                            MPI_Type_custom_reg_function *regfn,
                            MPI_Count reg_count, MPI_Datatype *type);
 
+/*
+ * All functions return 0 on success and non-zero on failure.
+ */
+typedef int (MPI_Type_custom2_pack_state_function)(
+    void *context, // Input context, as passed in create function
+    const void *src, // Source buffer to unpack from
+    MPI_Count count, // Number of elements in buffer; could represent bytes, element counts, etc.
+    void **state // State to be created for packing
+);
+typedef int (MPI_Type_custom2_unpack_state_function)(
+    void *context, // Input context, as passed in create function
+    void *dst, // Destination buffer
+    MPI_Count count, // Number of element in destination buffer; could represent bytes, element counts, etc.
+    void **state // State to be created for unpacking
+);
+typedef int (MPI_Type_custom2_packed_size_function)(
+    void *context, // Input context, as passed to create function
+    const void *buf, // User-provided buffer (not packed)
+    MPI_Count count, // Number of elements in buffer; could represent bytes, element counts, etc.
+    MPI_Count *size // Output number of bytes to be packed or expected on receive
+);
+typedef int (MPI_Type_custom2_pack_function)(
+    void *state, // State information for packing
+    MPI_Count offset, // Virtual offset in bytes into the packed buffer
+    void *dst, // Destination buffer
+    MPI_Count dst_size // Number of bytes to be written to destination buffer
+);
+typedef int (MPI_Type_custom2_unpack_function)(
+    void *state, // State information for unpacking
+    MPI_Count offset, // Virtual offset in bytes into the buffer being unpacked
+    const void *src, // Incoming buffer to be unpacked
+    MPI_Count src_size // Number of bytes in current buffer to be unpacked
+);
+typedef int (MPI_Type_custom2_pack_state_free_function)(void *state);
+typedef int (MPI_Type_custom2_unpack_state_free_function)(void *state);
+
+int MPI_Type_create_custom2(MPI_Type_custom2_pack_state_function *pack_statefn,
+                            MPI_Type_custom2_unpack_state_function *unpack_statefn,
+                            MPI_Type_custom2_packed_size_function *sizefn,
+                            MPI_Type_custom2_pack_function *packfn,
+                            MPI_Type_custom2_unpack_function *unpackfn,
+                            MPI_Type_custom2_pack_state_free_function *pack_freefn,
+                            MPI_Type_custom2_unpack_state_function *unpack_freefn,
+                            void *context, // Context pointer to be stored for initializing state
+                            MPI_Datatype *type);
+
 /* Idea: use a builder-like interface */
 
 /* Constants */
