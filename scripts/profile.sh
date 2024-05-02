fc2534af8a5371a1d5f7b5c74db918a65ac46b73
Jake Tronge <jtronge3@gmail.com>
2024-05-02 15:57:30 -0600

    Convert profiling script to use Rust flamegraph

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [ $OMPI_COMM_WORLD_RANK -eq $PROFILE_RANK ]; then
-    exec perf record -F 99 -g -- $@
+# Install flamegraph with `cargo install flamegraph`
+if [ $OMPI_COMM_WORLD_RANK -eq 0 ]; then
+    exec flamegraph -o out.svg -- $@
 else
     exec $@
 fi
--- scripts/profile_runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export PROFILE_RANK=0
-mpirun -np 2 ./scripts/profile.sh $@
-perf script > out.perf
-$FLAME_GRAPH_PATH/stackcollapse-perf.pl out.perf > out.stack
-$FLAME_GRAPH_PATH/flamegraph.pl out.stack > out.svg
