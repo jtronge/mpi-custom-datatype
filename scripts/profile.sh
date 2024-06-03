0359604cfb0a3ed97e604b92626b8b1a43fddf25
Jake Tronge <jtronge3@gmail.com>
2024-06-03 15:06:35 -0400

    Add flamegraph profiles for aarch64

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if [ -z "$FLAMEGRAPH_PATH" ]; then
-    printf "FLAMEGRAPH_PATH must be set in environment\n"
+if [ -z "$FLAMEGRAPH_PATH" -o -z "$FLAMEGRAPH_RANK" ]; then
+    printf "Both FLAMEGRAPH_PATH and $FLAMEGRAPH_RANK must be set in environment\n"
     exit 1
 fi
 
 # Install flamegraph with `cargo install flamegraph`
-exec flamegraph -o $FLAMEGRAPH_PATH-$OMPI_COMM_WORLD_RANK.svg -- $@
+if [ $OMPI_COMM_WORLD_RANK -eq $FLAMEGRAPH_RANK ]; then
+    exec flamegraph -o $FLAMEGRAPH_PATH-$OMPI_COMM_WORLD_RANK.svg -- $@
+else
+    exec $@
+fi
