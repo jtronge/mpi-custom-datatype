1ddd4c2d81b7a6c374e704f6331b1098a3e36082
Jake Tronge <jtronge3@gmail.com>
2024-06-13 18:47:26 -0400

    Add flamegraphs and new profiling script

---
+++ scripts/profile.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Profile an MPI job on all ranks."""
+import argparse
+import os
+from pathlib import Path
+import subprocess
+import tempfile
+import time
+import uuid
+
+
+def run_profile(args):
+    """Run a profile for the program, yielding a list of profile files."""
+    profile_path = Path('~/.profiles').resolve()
+    temp_dir = profile_path / uuid.uuid4().hex
+    os.makedirs(temp_dir)
+    print(f'profile dir: {temp_dir}')
+    sbatch_script = Path(temp_dir, 'sbatch.sh')
+    cmd_script = Path(temp_dir, 'command.sh')
+    # Generate the command script with perf wrapping the real program to be run
+    perf_cmd = f'perf record -F 997 --call-graph dwarf,16384 -g -o {temp_dir}/perf.$OMPI_COMM_WORLD_RANK'
+    with open(cmd_script, 'w') as cmd_fp:
+        cmd = ' '.join(args)
+        print('#!/bin/sh', file=cmd_fp)
+        print(f'exec {perf_cmd} -- {cmd}', file=cmd_fp)
+
+    # Create the sbatch script that invokes the command script
+    output_log = f'{temp_dir}/out.log'
+    with open(sbatch_script, 'w') as sbatch_fp:
+        print('#!/bin/sh', file=sbatch_fp)
+        print('#SBATCH -N 2', file=sbatch_fp)
+        print(f'#SBATCH -o {output_log}', file=sbatch_fp)
+        print(f'mpirun -np 2 -N 1 /bin/sh {cmd_script}', file=sbatch_fp)
+
+    # Now wait for the job to complete
+    try:
+        subprocess.run(['sbatch', '-W', sbatch_script])
+    except subprocess.CalledProcessError as err:
+        time.sleep(2)
+        with open(cmd_script) as cmd_fp:
+            print(cmd_fp.read())
+        with open(sbatch_script) as sbatch_fp:
+            print(sbatch_fp.read())
+        raise err
+
+    # Find all the perf outputs
+    for fname in os.listdir(temp_dir):
+        if not fname.startswith('perf.'):
+            continue
+        _, rank = fname.split('.')
+        yield temp_dir / fname, int(rank)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output', required=True, help='output directory for flamegraphs')
+    args, prog_args = parser.parse_known_args()
+
+    for profile, rank in run_profile(prog_args):
+        subprocess.run(['flamegraph', '--perfdata', profile, '-o',
+                        Path(args.output, f'flamegraph-{rank}.svg'), '--',
+                        prog_args[0]], check=True)
--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
+#
+# This script runs perf for each rank of an MPI job. Profile data is dumped to
+# $PROFILE_DIR; a tool, such as flamegraph, should be used after the job is
+# complete to visualize the results.
+#
+# Example command to run:
+#
+# `PROFILE_DIR=./struct-vec-profile-packed mpirun -np 2 -N 1 ./scripts/profile.sh ./target/release/mpicd_bw --kind packed --datatype struct-simple --options-path ./benchmark-options/bw-struct-simple-profile.yml --subvector-size 0`
+#
+# Then to generate a flamegraph:
+# flamegraph --perfdata $PROFILE_DIR/perf-0.out -- ./target/release/mpicd_bw
+#
 
-if [ -z "$FLAMEGRAPH_PATH" -o -z "$FLAMEGRAPH_RANK" ]; then
-    printf "Both FLAMEGRAPH_PATH and $FLAMEGRAPH_RANK must be set in environment\n"
+if [ -z "$PROFILE_DIR" ]; then
+    printf "PROFILE_DIR must be set in environment\n"
     exit 1
 fi
 
-# Install flamegraph with `cargo install flamegraph`
-if [ $OMPI_COMM_WORLD_RANK -eq $FLAMEGRAPH_RANK ]; then
-    exec flamegraph -o $FLAMEGRAPH_PATH-$OMPI_COMM_WORLD_RANK.svg -- $@
-else
-    exec $@
+if [ -z "$OMPI_COMM_WORLD_RANK" ]; then
+    printf "This script must be run with Open MPI's mpirun\n"
+    exit 1
 fi
+
+exec perf record -F 997 --call-graph dwarf,16384 -g -o $PROFILE_DIR/perf-$OMPI_COMM_WORLD_RANK.out -- $@
