4ed48981a591b209a285502215587a299c13e7b3
Jake Tronge <jtronge3@gmail.com>
2024-07-09 13:15:00 -0600

    Add Python results with NUMPY_MADVISE_HUGEPAGE=0

---
+++ scripts/pingpong-1-numpy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export UCX_TLS=rc
+export NUMPY_MADVISE_HUGEPAGE=0
+mpirun -np 2 python3 ./examples/pingpong.py > results/python-pingpong-numpy-var/one-node/baseline.out
+mpirun -np 2 python3 ./examples/pingpong.py -po > results/python-pingpong-numpy-var/one-node/pickle_oob.out
+mpirun -np 2 python3 ./examples/pingpong.py -p > results/python-pingpong-numpy-var/one-node/pickle_basic.out
