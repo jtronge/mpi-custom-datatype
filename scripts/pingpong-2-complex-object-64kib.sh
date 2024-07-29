131ace07405bcb544d5578076506d00a36bf8d5d
Jake Tronge <jtronge3@gmail.com>
2024-07-29 14:38:39 -0600

    Add 128 KiB results and run scripts

---
+++ scripts/pingpong-2-complex-object-64kib.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export UCX_NET_DEVICES="mlx5_0:1"
+export ARGS="-m 65536 -n 1073741824 --complex-object-size 65536"
+export RESULTS_PATH=results/python-pingpong-complex-object-64kib/two-node
+
+mkdir -p $RESULTS_PATH
+mpirun -np 2 -N 1 -- python3 ./examples/pingpong.py $ARGS \
+    > $RESULTS_PATH/baseline.out
+mpirun -np 2 -N 1 -- python3 ./examples/pingpong.py $ARGS -poc --array complex-object \
+    > $RESULTS_PATH/pickle_oob_cdt.out
+mpirun -np 2 -N 1 -- python3 ./examples/pingpong.py $ARGS -po --array complex-object \
+    > $RESULTS_PATH/pickle_oob.out
+mpirun -np 2 -N 1 -- python3 ./examples/pingpong.py $ARGS -p --array complex-object \
+    > $RESULTS_PATH/pickle_basic.out
